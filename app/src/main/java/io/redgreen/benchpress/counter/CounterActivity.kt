b43405e128df9e4d5174418a670ed690253c2c3c
Deepankur-Sadana <writetodeepankursadana@gmail.com>
2019-04-03 02:28:08 +0530

    update function pending

--- app/src/main/java/io/redgreen/benchpress/counter/CounterActivity.kt
+++ app/src/main/java/io/redgreen/benchpress/counter/CounterActivity.kt
@@ -7,32 +7,47 @@ import com.spotify.mobius.Next
 import io.reactivex.ObservableTransformer
 import io.redgreen.benchpress.R
 import io.redgreen.benchpress.architecture.android.BaseActivity
+import io.redgreen.benchpress.architecture.threading.DefaultSchedulersProvider
+import io.redgreen.benchpress.counter.effectHandler.ZzzEffectHandler
 import kotlinx.android.synthetic.main.counter_activity.*
 
-class CounterActivity : BaseActivity<CounterModel, CounterEvent, ZzzEffect>(), CounterActions {
+class CounterActivity :
+    BaseActivity<CounterModel, CounterEvent, ZzzEffect>(),
+    CounterActions {
+
     override fun layoutResId(): Int {
         return R.layout.counter_activity
     }
 
     override fun setup() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        setupViewEventListeners()
     }
 
-    override fun initialModel(): CounterModel {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private fun setupViewEventListeners() {
+        incrementButton.setOnClickListener { eventSource.notifyEvent(IncrementEvent) }
+        decrementButton.setOnClickListener { eventSource.notifyEvent(DecrementEvent) }
+
     }
 
-    override fun updateFunction(model: CounterModel, event: CounterEvent): Next<CounterModel, ZzzEffect> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    //todo return zero or from saved state ???
+    override fun initialModel(): CounterModel =
+        CounterModel.ZERO
+
+
+    override fun updateFunction(
+        model: CounterModel,
+        event: CounterEvent
+    ): Next<CounterModel, ZzzEffect> {
+        TODO()
     }
 
     override fun render(model: CounterModel) {
         counterTextView.text = model.counter.toString()
     }
 
-    override fun effectHandler(): ObservableTransformer<ZzzEffect, CounterEvent> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun effectHandler(): ObservableTransformer<ZzzEffect, CounterEvent> =
+        ZzzEffectHandler.create(this, DefaultSchedulersProvider())
+
 
     companion object {
         fun start(context: Context) {
@@ -41,14 +56,14 @@ class CounterActivity : BaseActivity<CounterModel, CounterEvent, ZzzEffect>(), C
     }
 
     override fun showFizz() {
-        Toast.makeText(this,"Fizz",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Fizz", Toast.LENGTH_SHORT).show()
     }
 
     override fun showBuzz() {
-        Toast.makeText(this,"Buzz",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Buzz", Toast.LENGTH_SHORT).show()
     }
 
     override fun showFizzBuzz() {
-        Toast.makeText(this,"FizzBuzz",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "FizzBuzz", Toast.LENGTH_SHORT).show()
     }
 }
